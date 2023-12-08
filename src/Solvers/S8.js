83d5157b28d78b28f9f23c526831b76bf5cebf63
pmanlig <patrik@manlig.org>
2023-12-08 07:03:12 +0100

    Day 8

--- src/Solvers/S8.js
+++ src/Solvers/S8.js
@@ -4,6 +4,58 @@ import Solver from './Solvers';
 
 export class S8 extends Solver {
 	solve(input) {
-		return { solution: `Ingen lösning än` };
+		function countSteps(directions, map) {
+			const start = "AAA";
+			const end = "ZZZ";
+
+			let current = start, steps = 0;
+			while (current !== end) {
+				current = directions[steps % directions.length] === "R" ? map[current].right : map[current].left;
+				steps++;
+			}
+
+			return steps;
+		}
+
+		function countGhostSteps(directions, map) {
+			function isEnd(c) { return c[2] === "Z"; }
+
+			function findLoop(start) {
+				let current = start, steps = 0;
+				while (!isEnd(current)) {
+					current = directions[steps % directions.length] === "R" ? map[current].right : map[current].left;
+					steps++;
+				}
+				return steps;
+			}
+
+			let loop = Object.keys(map).filter(k => k[2] === "A").map(c => findLoop(c));
+			let steps = loop[0], increment = loop[0], current = 1;
+			while (current < loop.length) {
+				while (steps % loop[current] !== 0) { steps += increment; }
+				current++;
+				increment = steps;
+			}
+
+			return steps;
+		}
+
+		// input = "RL\n\nAAA = (BBB, CCC)\nBBB = (DDD, EEE)\nCCC = (ZZZ, GGG)\nDDD = (DDD, DDD)\nEEE = (EEE, EEE)\nGGG = (GGG, GGG)\nZZZ = (ZZZ, ZZZ)"
+		// input = "LLR\n\nAAA = (BBB, BBB)\nBBB = (AAA, ZZZ)\nZZZ = (ZZZ, ZZZ)";
+		let directions = input.match(/^([RL]+)/)[0].split('');
+		let map = {}
+		input.split('\n').slice(2).map(l => l.match(/^([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)/).slice(1)).forEach(m => {
+			// console.log("parsing", m);
+			map[m[0]] = {
+				left: m[1],
+				right: m[2]
+			}
+		});
+		// console.log(directions, map);
+
+		let sol1 = countSteps(directions, map);
+		let sol2 = countGhostSteps(directions, map);
+
+		return { solution: `Steps: ${sol1}\nGhost steps: ${sol2}` };
 	}
 }
