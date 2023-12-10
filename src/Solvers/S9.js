65d689e44bf67f58976719c2f1549f7c4e795355
pmanlig <patrik@manlig.org>
2023-12-10 08:05:22 +0100

    Day 9

--- src/Solvers/S9.js
+++ src/Solvers/S9.js
@@ -4,6 +4,48 @@ import Solver from './Solvers';
 
 export class S9 extends Solver {
 	solve(input) {
-		return { solution: `Ingen lösning än` };
+		function extrapolate(vals) {
+			let extra = [vals];
+			let lev = 0;
+			while (extra[lev].some(x => x !== 0)) {
+				extra.push([]);
+				for (let i = 1; i < extra[lev].length; i++) {
+					extra[lev + 1].push(extra[lev][i] - extra[lev][i - 1]);
+				}
+				lev++;
+			}
+			extra[lev--].push(0);
+			while (lev >= 0) {
+				extra[lev].push(extra[lev + 1].pop() + extra[lev].pop());
+				lev--;
+			}
+			return extra[0].pop();
+		}
+
+		function backpolate(vals) {
+			let extra = [vals];
+			let lev = 0;
+			while (extra[lev].some(x => x !== 0)) {
+				extra.push([]);
+				for (let i = 1; i < extra[lev].length; i++) {
+					extra[lev + 1].push(extra[lev][i] - extra[lev][i - 1]);
+				}
+				lev++;
+			}
+			let back = 0;
+			while (--lev >= 0) {
+				back = extra[lev][0] - back;
+			}
+			return back;
+		}
+
+		// input = "0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45";
+		input = input.split('\n').map(l => l.split(' ').map(n => parseInt(n, 10)));
+
+		// console.log(input);
+
+		let sol1 = input.map(l => extrapolate(l)).reduce((a, b) => a + b);
+		let sol2 = input.map(l => backpolate(l)).reduce((a, b) => a + b);
+		return { solution: `Extrapolated values: ${sol1}\nBackwards extrapolated values: ${sol2}` };
 	}
 }
